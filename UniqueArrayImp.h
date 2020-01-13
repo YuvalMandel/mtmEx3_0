61047d677357d5b0a725807b2f51247399b8112b
Yuval Mandel <tubal.mandel.212@gmail.com>
2020-01-13 20:00:14 +0200

    compiled functions

--- UniqueArrayImp.h
+++ UniqueArrayImp.h
@@ -161,7 +161,7 @@ UniqueArray<Element, Compare> UniqueArray<Element, Compare>::filter
 
     for(int i=0; i<array_size; i++)
     {
-        if(filter(array[i]))
+        if(f(*array[i]))
 
             new_array.array[i]=array[i];
     }
