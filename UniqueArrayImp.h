e123ff10c3d0a8fb8d6c42324d50c7507e2d2017
Yuval Mandel <tubal.mandel.212@gmail.com>
2020-01-26 22:41:00 +0200

    added UniqeArrayImp comments and checked uniqearray with original tests

--- UniqueArrayImp.h
+++ UniqueArrayImp.h
@@ -3,6 +3,12 @@
 
 //**********************************************************************
 template<class Element, class Compare>
+/**
+ * UniqueArray constructor
+ *
+ * creates array of pointers to Elements. dynamic allocation occurs only
+ * when an element is received.
+ */
 UniqueArray<Element, Compare>::UniqueArray(unsigned int size):
         array_size(size), num_of_element(0)
 {
@@ -19,6 +25,13 @@ UniqueArray<Element, Compare>::UniqueArray(unsigned int size):
 
 //**********************************************************************
 template<class Element, class Compare>
+
+/**
+* UniqueArray copy constructor
+*
+* creates array of pointers to Elements. for each element, use the element
+ * copy C'tor to create a new copied Element.
+*/
 UniqueArray<Element, Compare>::UniqueArray(const UniqueArray &other) {
 
     array_size = other.array_size;
@@ -27,13 +40,24 @@ UniqueArray<Element, Compare>::UniqueArray(const UniqueArray &other) {
 
     for (unsigned int i=0; i<other.array_size; i++)
     {
-        array[i] = new Element(*other.array[i]);
+        if (other.array[i]!= nullptr) {
+            array[i] = new Element(*other.array[i]);
+        }
+        else{
+            array[i]= nullptr;
+        }
     }
 
 }
 
 //**********************************************************************
 template<class Element, class Compare>
+
+
+/**
+* UniqueArray copy D'tor
+*
+*/
 UniqueArray<Element, Compare>::~UniqueArray() {
 
     for(unsigned int i = 0; i <array_size; i++)
@@ -51,6 +75,13 @@ UniqueArray<Element, Compare>::~UniqueArray() {
 
 //**********************************************************************
 template<class Element, class Compare>
+
+
+/**
+* returns true if an element is in the array. false otherwise. uses type:
+ * Compare to decide whether two elements are in fact equal. if equality is
+ * found the index of the item in the array is returned in param "index".
+*/
 bool UniqueArray<Element, Compare>::getIndex(const Element &element,
                                              unsigned int &index) const {
     Compare c;
@@ -76,7 +107,9 @@ bool UniqueArray<Element, Compare>::getIndex(const Element &element,
 }
 
 //**********************************************************************
-
+/**
+* inserts an item to the array if it does not already exist there.
+*/
 template<class Element, class Compare>
 unsigned int UniqueArray<Element, Compare>::insert(const Element &element) {
 
@@ -104,6 +137,10 @@ unsigned int UniqueArray<Element, Compare>::insert(const Element &element) {
 
 //**********************************************************************
 template<class Element, class Compare>
+/**
+*if the element received it equal to an element in the array (equal defined
+ * by Compare) a pointer to  the element in the array is returned.
+*/
 const Element *
 UniqueArray<Element, Compare>::operator[](const Element &element) const {
 
@@ -119,6 +156,10 @@ UniqueArray<Element, Compare>::operator[](const Element &element) const {
 
 //**********************************************************************
 template<class Element, class Compare>
+/**
+*if the element exists in the array it is removed. true or false returned to
+ * indicate the successful removal of element.
+*/
 bool UniqueArray<Element, Compare>::remove(const Element &element) {
 
     unsigned int int_temp;
@@ -140,6 +181,9 @@ bool UniqueArray<Element, Compare>::remove(const Element &element) {
 
 //**********************************************************************
 template<class Element, class Compare>
+/**
+*returnes current amount of elements stored in array
+*/
 unsigned int UniqueArray<Element, Compare>::getCount() const {
 
     return num_of_element;
@@ -149,6 +193,9 @@ unsigned int UniqueArray<Element, Compare>::getCount() const {
 
 //**********************************************************************
 template<class Element, class Compare>
+/**
+*returns the maximum number of items that can be stored in array.
+*/
 unsigned int UniqueArray<Element, Compare>::getSize() const {
 
     return array_size;
@@ -160,6 +207,12 @@ unsigned int UniqueArray<Element, Compare>::getSize() const {
 
 //**********************************************************************
 template<class Element, class Compare>
+/**
+ * creates a new  Unique array containing only the a elements of the current
+ * array that when given to funtion: filter, return true.  the index of these
+ * items in the new filtered array is identical to their index in the
+ * original array.
+*/
 UniqueArray<Element, Compare> UniqueArray<Element, Compare>::filter
 (const UniqueArray::Filter &f) const {
 
@@ -185,6 +238,9 @@ UniqueArray<Element, Compare> UniqueArray<Element, Compare>::filter
 
 //**********************************************************************
 template<class Element, class Compare>
+/**
+*returnes a pointer to the element in index: "index" if the array.
+*/
 Element *UniqueArray<Element, Compare>::getElementByIndex(int index) const {
 
     return array[index];
